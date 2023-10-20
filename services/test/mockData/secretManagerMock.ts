e5dbd69491b6ef50c73b7675c418dc76922e17eb
Alexander Hauenstein <a.hauenstein@innfactory.de>
2023-10-20 11:24:37 +0200

    fix: secret mock

--- services/test/mockData/secretManagerMock.ts
+++ services/test/mockData/secretManagerMock.ts
@@ -1,3 +1,10 @@
+import { ApiKeySecret } from '@common/secretmanager/domain/models/apiKeySecret';
+
+const migrationsApiKey: ApiKeySecret = {
+	apiKey: 'test-api-key',
+	version: 1,
+};
+
 export const secretManagerMock: { [key: string]: unknown } = {
-	'migrations-api-key': 'test-api-key',
+	'migrations-api-key': migrationsApiKey,
 };
